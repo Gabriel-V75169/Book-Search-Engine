cea7f85a40dee1d5c88b988404e843d799a89806
Gabriel Valencia <jrjr75169@gmail.com>
2023-12-08 23:39:12 -0800

    Final

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,35 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Navbar from './components/Navbar';
+
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+});
+
+const authLink = setContext((_, { headers }) => {
+
+  const token = localStorage.getItem('id_token');
+  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const typeDefs =
+`
+type Query {
+    me: User
+
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+}
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    authors: [String]
+    description: String
+    bookId: ID!
+    title: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+input BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+`;
+
+module.exports = typeDefs;
